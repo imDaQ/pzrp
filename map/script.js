84396149262e34f8c3f04955e1402a99654b7cfa
imDaQ <gangst3rok@gmail.com>
2022-06-26 10:16:54 +0300

    пива

--- map/script.js
+++ map/script.js
@@ -1,63 +1,110 @@
-// the global image size and tiling
-const height = 3500;
-const width = 6950;
-const tsize = 500;
-
-// initialize the openseadragon viewer
-const osd = OpenSeadragon({
-  id: 'osd',
-  prefixUrl: 'https://openseadragon.github.io/openseadragon/images/',
-  tileSources: {
-    type: 'image',
-    url: 'map2.png'
+var baseTileSource = {
+  Image: {
+    xmlns: 'http://schemas.microsoft.com/deepzoom/2008',
+    Url: 'http://openseadragon.github.io/example-images/highsmith/highsmith_files/',
+    Format: 'jpg',
+    Overlap: '2',
+    TileSize: '256',
+    Size: {
+      Height: '10500',
+      Width: '20850'
+    }
   }
-  });
+};
+var topTileSource = {
+  type: 'image',
+  url: 'https://i.imgur.com/eON2wQF.png',
+
+};
+var viewer = OpenSeadragon({
+  id: "osd",
+  prefixUrl: "https://cdnjs.cloudflare.com/ajax/libs/openseadragon/2.4.1/images/",
+  tileSources: [baseTileSource,topTileSource],
+     showNavigator:  true
+});
 
 
-// initialize the geojs viewer
-const params = geo.util.pixelCoordinateParams('#geojs', width, height, tsize, tsize);
+
+
+
+
+//showNavigator:  true
+// viewer.maxZoomPixelRatio = 50;
+
+var params = geo.util.pixelCoordinateParams('#geojs', 7026, 9221, 256, 256);
+params.map.max = 16;
 params.map.clampZoom = false;
 params.map.clampBoundsX = false;
 params.map.clampBoundsY = false;
-const map = geo.map(params.map);
-const layer = map.createLayer('annotation');
+var map = geo.map(params.map);
+var layer=map.createLayer('annotation');
 
 // turn off geojs map navigation
-map.interactor().options({ actions: [] });
+map.interactor().options({
+  actions: []
+});
 
-// get the current bounds from the osd viewer
+// get the current bounds from the viewer viewer
 function getBounds() {
-  return osd.viewport.viewportToImageRectangle(osd.viewport.getBounds(true));
+  //console.log(viewer.TiledImage);
+  //console.log(OpenSeadragon.TiledImage);
+  return viewer.viewport.viewportToImageRectangle(viewer.viewport.getBounds(true));
+  //return viewer.world.getItemAt(0).viewportToImageRectangle(viewer.world.getItemAt(0).getBounds(true));
 }
 
-// set the geojs bounds from the osd bounds
+
+
+
+// set the geojs bounds from the viewer bounds
+
 function setBounds() {
-  const bounds = getBounds();
+  var bounds = getBounds();
+  console.log(bounds);
   map.bounds({
     left: bounds.x,
     right: bounds.x + bounds.width,
     top: bounds.y,
-    bottom: bounds.y + bounds.height });
-
+    bottom: bounds.y + bounds.height
+  });
 }
 
 // add handlers to tie navigation events together
-osd.addHandler('open', setBounds);
-osd.addHandler('animation', setBounds);
+
+viewer.addHandler('open', setBounds);
+viewer.addHandler('animation', setBounds);
+
+
+
 
 // add a handler for when an annotation is created
 function created(evt) {
-  $('#geojs .geojs-layer').css('pointer-events', 'none');
+  $('#geojs .geojs-layer').css('pointer-events', 'none'); // write out the annotation definition
 
-  // write out the annotation definition
-  console.log(evt.annotation.features()[0]);
+  //console.log(evt.annotation.features()[0]);
 }
-map.geoOn(geo.event.annotation.state, created);
 
-// add handlers for drawing annotations
+map.geoOn(geo.event.annotation.state, created); // add handlers for drawing annotations
+
 function draw(evt) {
   $('#geojs .geojs-layer').css('pointer-events', 'auto');
-  const type = $(evt.target).data('type');
+  var type = $(evt.target).data('type');
   layer.mode(type);
 }
+
 $('.controls-container button').click(draw);
+
+
+$(".niki-wrapper #layer-01").on("input",function (){
+  var val = $(this).val()
+   viewer.world.getItemAt(1).setOpacity(val);
+  test();
+});
+
+
+
+
+viewer.addHandler('tile-loaded', function(event) {
+  console.log(event)
+  var canvas = document.createElement( 'canvas' )
+  } ) 
+                   
